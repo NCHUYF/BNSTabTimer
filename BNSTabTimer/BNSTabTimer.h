8bc566307686bfa35098c57ff783987aada2e896
‘YanF’ <744539997@qq.com>
2022-10-22 00:10:04 +0800

    主窗口实现

---
+++ BNSTabTimer/BNSTabTimer.h
@@ -0,0 +1,37 @@
+﻿
+// BNSTabTimer.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+// 程序单实例
+#define BNSTabTimer_CLASSNAME _T("BNSTabTimer")
+#define APPMUTEX _T("Global\\ScanSettings")
+
+
+// CBNSTabTimerApp:
+// 有关此类的实现，请参阅 BNSTabTimer.cpp
+//
+
+class CBNSTabTimerApp : public CWinApp
+{
+public:
+	CBNSTabTimerApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+	BOOL RestrictOneInstance();
+DECLARE_MESSAGE_MAP()
+private:
+	HANDLE m_hMutex;
+};
+
+extern CBNSTabTimerApp theApp;
